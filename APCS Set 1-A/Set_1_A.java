72f1acf068a49c570cb668a2c30cd58b671edc7f
Dwdw621 <55766994+Dwdw621@users.noreply.github.com>
2021-09-21 10:44:55 -0700

    Add files via upload

--- APCS Set 1-A/Set_1_A.java
+++ APCS Set 1-A/Set_1_A.java
@@ -14,10 +14,13 @@ public class Set_1_A{
        double a = 1; 
        double b = 5; 
        double c = 6; 
+       
+       //Calculation
+       double root1 = (-b-Math.sqrt(b*b-4*a*c))/(2*a);
+       double root2 = (-b+Math.sqrt(b*b-4*a*c))/(2*a);
         
        System.out.println("QUADRATIC FORMULA");
-       System.out.println("The solutions for " + (int)a + "x^2 + " + (int)b + "x + " + (int)c
-       + " are " + (-b-Math.sqrt(b*b-4*a*c))/(2*a) + " and " + (-b+Math.sqrt(b*b-4*a*c))/(2*a) + ".\n");  
+       System.out.println("The solutions for " + (int)a + "x^2 + " + (int)b + "x + " + (int)c + " are " + root1 + " and " + root2 + ".\n");  
        
        
        
@@ -30,11 +33,11 @@ public class Set_1_A{
        double bx= 2;
        double by= 3;
        
-       //Slope (Rise/Run)
-       double m = (by-ay)/(bx-ax);
+       //Calculation
+       double s = (by-ay)/(bx-ax);
       
        System.out.println("SLOPE FORMULA");
-       System.out.println("A line connecting the points (" + ax + ", " + ay + ") and (" + bx + ", " + by + ") has a slope of " + m  + ".\n");
+       System.out.println("A line connecting the points (" + ax + ", " + ay + ") and (" + bx + ", " + by + ") has a slope of " + s  + ".\n");
 
        
        
@@ -48,7 +51,7 @@ public class Set_1_A{
        double dx= 2;
        double dy= 3;
        
-       //Midpoint (mx, my)
+       //Calculation
        double mx = (ax+bx)/2; 
        double my = (ay+by)/2;
        
@@ -57,30 +60,30 @@ public class Set_1_A{
        
        
        
-            //Sum of Arithmetic Series
+            //Sum of an Arithmetic Series
        //Input
        double a1 = 1; //First term
-       double d = 1; //Common difference between each term
+       double d = 1;  //Common difference between each term
        double an = 5; //Number of terms
        
        //Calculation
        double SA = an/2*(2*a1+(an-1)*d); //Sum of the arithmetic series
        
-       System.out.println("SUM OF ARITHMETIC SERIES");
+       System.out.println("SUM OF AN ARITHMETIC SERIES");
        System.out.println("The sum of the first " + (int)an + " terms of an arithmetic series that starts with " + a1 + " and increases by " + d + " is " + SA  + ".\n");
        
        
        
-            //Sum of Geometric Series
+            //Sum of a Finite Geometric Series
        //Input
        double g1 = 3; //First term
-       double q = 2; //Common quotient between each term
+       double r = 2;  //Common ratio between each term
        double gn = 3; //Number of terms
        
        //Calculation
-       double SG = (g1*(1- Math.pow(q,gn)))/(1-q); //Sum of the arithmetic series
+       double SG = (g1*(1- Math.pow(r,gn)))/(1-r); //Sum of the geometric series
        
        System.out.println("SUM OF A FINITE ARITHMETIC SERIES");
-       System.out.println("The sum of the first " + (int)gn + " terms of a finite geometric series that starts with " + g1 + " and increases by a rate of " + q + " is " + SG  + ".\n");
+       System.out.println("The sum of the first " + (int)gn + " terms of a finite geometric series that starts with " + g1 + " and increases by a rate of " + r + " is " + SG  + ".\n");
     } 
-    }
+}
